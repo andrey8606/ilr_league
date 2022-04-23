e7e64a5c57d22f292ac022b9a6252328438c18d7
andrey8606 <andrey8606@gmail.com>
2022-04-23 14:32:27 +0300

    index changes

--- ilr_league/settings.py
+++ ilr_league/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ilr_league.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
