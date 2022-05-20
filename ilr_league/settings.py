f8fd8a448e3fe16d34058547f48fd13d0d92189a
andrey8606 <andrey8606@gmail.com>
2022-05-20 11:15:30 +0300

    city v2

--- ilr_league/settings.py
+++ ilr_league/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False')
+# DEBUG = os.environ.get('DEBUG', 'False')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
